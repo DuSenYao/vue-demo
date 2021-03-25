d66e95a020bf83ba9f6ea52c9050af39cde9263e
1758231591 <1758231591@qq.com>
2021-03-26 01:11:29 +0800

    create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// const path = require("path");
+// function resolve(dir) {
+//   return path.join(__dirname, "./", dir);
+// }
+
+module.exports = {
+  publicPath: "/",
+  outputDir: "dist",
+  assetsDir: "",
+  runtimeCompiler: true,
+  productionSourceMap: false
+};
