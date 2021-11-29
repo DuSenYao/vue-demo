a273d68d776ff9145621118c3d3caa27379d03e9
dusenyao <1758231591@qq.com>
2021-11-29 09:55:09 +0800

    修改 eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/singleline-html-element-content-newline': 0,
+    'vue/multi-word-component-names': 0,
     'vue/max-attributes-per-line': [
       2,
       {
--- src/views/user/login/index.vue
+++ src/views/user/login/index.vue
@@ -2,7 +2,9 @@
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" label-position="left">
       <div class="title-container">
-        <h3 class="title">系统登录</h3>
+        <h3 class="title">
+          系统登录
+        </h3>
       </div>
 
       <el-form-item prop="username">
